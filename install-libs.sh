3c51c5fba2dc39efadae602c5b6e4d4854164f77
Ace Medlock <ace.medlock@gmail.com>
2016-10-02 14:00:34 +0200

    some temporary local tooling

---
+++ install-libs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p /home/eric/Arduino/libraries/src
+cp -v src/*.cpp src/*.h ~/Arduino/libraries/src/
---
+++ launch-arduino-ide.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+~/arduino-1.6.5-r5/arduino > ide.out 2>&1 &
+tail -f ide.out
+
