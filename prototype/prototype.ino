4f928f773aa2c5f1a18be80b051f41bfcea18f3b
Eric Herman <eric@freesa.org>
2017-07-22 18:25:44 +0200

    fix buffer size, slow down debug printing, lindent

--- prototype/prototype.ino
+++ prototype/prototype.ino
@@ -12,7 +12,7 @@ using namespace ADS1299;
 const int LED_PIN = 13;
 const unsigned Max_samples_no_leads = 250 * 5;
 const unsigned maxChannels = 8;
-const size_t num_ads_chips = (maxChannels/8);
+const size_t num_ads_chips = (maxChannels / 8);
 
 Data_frame frames[num_ads_chips];
 unsigned long sampleCount;
@@ -25,45 +25,44 @@ SdFile file;
 char fileName[80];
 int sdChipSelect = 2;
 
+void setup()
+{
+	pinMode(LED_PIN, OUTPUT);
+	digitalWrite(LED_PIN, LOW);
 
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);		
+	// Open serial communications
+	Serial.begin(115200);
 
-  // Open serial communications
-  Serial.begin(115200);
+	// Initialize OpenHardwareExG board
+	ADS129x.init();
 
-  // Initialize OpenHardwareExG board
-  ADS129x.init();
+	// init sd card
+	sampleCount = 0;
+	recording = 0;
+	samplesNoLeads = Max_samples_no_leads;
+	loopCounter = 0;
 
-  // init sd card
-  sampleCount = 0;
-  recording = 0;
-  samplesNoLeads = Max_samples_no_leads;
-  loopCounter = 0;
-
-  // Initialize at the highest speed supported by the board that is
-  // not over 50 MHz. Try a lower speed if SPI errors occur.
-  //  sd.begin(chipSelect, SPI_SPEED);
-  if (!sd.begin(sdChipSelect, SD_SCK_MHZ(50))) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    return;
-  } else {
-    Serial.println("Wiring is correct and a card is present.");
-  }    
+	// Initialize at the highest speed supported by the board that is
+	// not over 50 MHz. Try a lower speed if SPI errors occur.
+	//  sd.begin(chipSelect, SPI_SPEED);
+	if (!sd.begin(sdChipSelect, SD_SCK_MHZ(50))) {
+		Serial.println("initialization failed. Things to check:");
+		Serial.println("* is a card inserted?");
+		Serial.println("* is your wiring correct?");
+		Serial.println("* is sdChipSelect pin correct?");
+	} else {
+		Serial.println("Wiring is correct and a card is present.");
+	}
 }
 
-bool leads_on(Data_frame *frames, size_t len)
+bool leads_on(Data_frame * frames, size_t len)
 {
 	bool pos_found = false;
 	bool neg_found = false;
 
-	for(size_t i =0; i< len; ++i) { 
+	for (size_t i = 0; i < len; ++i) {
 		Data_frame frame = frames[i];
-		for(size_t j = 0; j < 8; ++j) {
+		for (size_t j = 0; j < 8; ++j) {
 			pos_found = pos_found || frame.loff_statp(j);
 			neg_found = neg_found || frame.loff_statn(j);
 			if (pos_found && neg_found) {
@@ -77,13 +76,13 @@ bool leads_on(Data_frame *frames, size_t len)
 
 size_t as_hex_unsafe(char *buf, void *data, size_t len)
 {
-  size_t i;
+	size_t i;
 	unsigned char *cp = (unsigned char *)data;
-	for(i = 0; i < len; ++i) {
-		to_hex(*(cp + i), buf + (i*2));
+	for (i = 0; i < len; ++i) {
+		to_hex(*(cp + i), buf + (i * 2));
 	}
-	buf[i*2] = '\0';
-	return i*2;
+	buf[i * 2] = '\0';
+	return i * 2;
 }
 
 void error(const char *msg1, const char *msg2)
@@ -96,90 +95,92 @@ void error(const char *msg1, const char *msg2)
 
 void open_file()
 {
-  unsigned long time = micros();
-  size_t i = 0;
+	unsigned long time = micros();
+	size_t i = 0;
 
 /*
-  fileName[i++] = 'e';
-  fileName[i++] = 'x';
-  fileName[i++] = 'g';
-  fileName[i++] = '_';
-  */
-  i += as_hex_unsafe(fileName+i, &time, sizeof(time));
-  fileName[i++] = '.';
-  fileName[i++] = 'd';
-  fileName[i++] = 'a';
-  fileName[i++] = 't';
-  fileName[i++] = '\0';
-  
-  if (!file.open(fileName, O_CREAT | O_WRITE | O_EXCL)) {
-    error("file.open", fileName);
-  } else {
-    Serial.print("opened ");
-    Serial.println(fileName);
-  }
+	fileName[i++] = 'e';
+	fileName[i++] = 'x';
+	fileName[i++] = 'g';
+	fileName[i++] = '_';
+*/
+	i += as_hex_unsafe(fileName + i, &time, sizeof(time));
+	fileName[i++] = '.';
+	fileName[i++] = 'd';
+	fileName[i++] = 'a';
+	fileName[i++] = 't';
+	fileName[i++] = '\0';
+
+	if (!file.open(fileName, O_CREAT | O_WRITE | O_EXCL)) {
+		error("file.open", fileName);
+	} else {
+		Serial.print("opened ");
+		Serial.println(fileName);
+	}
 }
 
 void close_file()
 {
-    file.close();
-    Serial.print("closed ");
-    Serial.println(fileName);
+	file.close();
+	Serial.print("closed ");
+	Serial.println(fileName);
 }
 
 void log_frames_sd()
 {
-  char buf[(2*9*3)+3];
-  file.print(sampleCount);
+	char buf[(2 * (3 + 3 * 8)) + 3];
+	file.print(sampleCount);
 
-  for (size_t i = 0; i < num_ads_chips; ++i) {
-     file.write(':');
-     Data_frame frame = frames[i];
-     as_hex_unsafe(buf, frame.data, frame.size);
-     file.print(buf);
-  }
-  file.println();
+	for (size_t i = 0; i < num_ads_chips; ++i) {
+		file.write(':');
+		Data_frame frame = frames[i];
+		as_hex_unsafe(buf, frame.data, frame.size);
+		file.print(buf);
+	}
+	file.println();
 }
 
 void notify_frames_arrival()
 {
 }
 
-void loop() {
-   if((loopCounter++ % 100000) == 0) {
-    Serial.print("loop count: ");
-    Serial.print(loopCounter);
-    if (recording) {
-      Serial.println(" (recording)");
-    } else {
-      Serial.print(" ADS Chip ID: ");
-      Serial.println(ADS129x.chipId());
-    }
-   }
-    
-    if (ADS129x.getFrames(frames, num_ads_chips)) {
-	if (leads_on(frames, num_ads_chips)) {
-		samplesNoLeads = 0;
-	} else if (samplesNoLeads < Max_samples_no_leads) {
-		samplesNoLeads++;
+void loop()
+{
+	if ((loopCounter % 1000000) == 0) {
+		Serial.print("loop count: ");
+		Serial.print(loopCounter);
+		if (recording) {
+			Serial.println(" (recording)");
+		} else {
+			Serial.print(" ADS Chip ID: ");
+			Serial.println(ADS129x.chipId());
+		}
 	}
+	loopCounter++;
 
-	if (!recording && samplesNoLeads == 0) {
-		open_file();
-		recording = true;
-		digitalWrite(LED_PIN, HIGH);
-	} else if(recording && samplesNoLeads > Max_samples_no_leads) {
-		close_file();
-		recording = false;
-		digitalWrite(LED_PIN, LOW);		
-		sampleCount = 0;
-	}
+	if (ADS129x.getFrames(frames, num_ads_chips)) {
+		if (leads_on(frames, num_ads_chips)) {
+			samplesNoLeads = 0;
+		} else if (samplesNoLeads < Max_samples_no_leads) {
+			samplesNoLeads++;
+		}
 
-	if (recording) {
-		sampleCount++;
-       		log_frames_sd();
-		// notify_frames_arrival();
+		if (!recording && samplesNoLeads == 0) {
+			open_file();
+			recording = true;
+			digitalWrite(LED_PIN, HIGH);
+		} else if (recording && samplesNoLeads > Max_samples_no_leads) {
+			close_file();
+			recording = false;
+			digitalWrite(LED_PIN, LOW);
+			sampleCount = 0;
+		}
+
+		if (recording) {
+			sampleCount++;
+			log_frames_sd();
+			// notify_frames_arrival();
+		}
 	}
-    }
-    // do_blue_tooth_stuff();
+	// do_blue_tooth_stuff();
 }
